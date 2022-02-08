eb9ce6058b7ccc96dbcf48497ddf0b745aca57c9
yemmyharry <yemmyharry@gmail.com>
2022-02-08 22:05:04 +0100

    add model for deck and cards

--- models/models.go
+++ models/models.go
@@ -1,1 +1,14 @@
 package models
+
+type Deck struct {
+	ID        string `json:"deck_id"`
+	Shuffled  bool   `json:"shuffled"`
+	Remaining int    `json:"remaining"`
+	Cards     []Card `json:"cards"`
+}
+
+type Card struct {
+	Value string `json:"value"`
+	Suit  string `json:"suit"`
+	Code  string `json:"code"`
+}

4dd1ba0168a38920bd003c88a6165d6f68d7b7a0
yemmyharry <yemmyharry@gmail.com>
2022-02-09 19:19:23 +0100

    setup routes with app

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"deckOfCards/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 
 	app := fiber.New()
+	routes.Setup(app)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")

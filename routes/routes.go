fe7d6ed921989dc64e233e1d5b4d843e80ad516b
yemmyharry <yemmyharry@gmail.com>
2022-02-10 01:46:53 +0100

    made routes easier to understand

--- routes/routes.go
+++ routes/routes.go
@@ -10,8 +10,8 @@ func Setup(app *fiber.App) {
 	deck := controllers.DeckController{DeckService: make(map[string]*models.Deck)}
 	api := app.Group("/api")
 	{
-		api.Get("/", deck.CreateDeck)
-		api.Get("/:deck_id", deck.OpenDeck)
-		api.Get("/:deck_id/draw", deck.DrawCard)
+		api.Get("/create", deck.CreateDeck)
+		api.Get("/open/:deck_id", deck.OpenDeck)
+		api.Get("/draw/:deck_id", deck.DrawCard)
 	}
 }
